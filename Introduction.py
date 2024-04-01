7979d905bc5269bafa59305c8f65c43f4f7d75a4
Maurício Goi Lacerda <32021294+maupnx@users.noreply.github.com>
2024-04-01 14:09:55 -0300

    Add files via upload
    Upload of Introdution.py file

---
+++ Introduction.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="Maurício's Portfolio", page_icon=":tada:", layout="wide")
+st.title("KINDA A DATA-SOLVER-PROBLEM")
+
+
+#-----HEADER SECTION----
+st.subheader("Marketing Data Science | Martech | Growth Lead | Data Analytics Lead | Data Strategist")
+st.write("") 
+st.image("mau-profile.png", width = 350)
+st.write("")
+st.subheader("LIFELONG LEARNERCURIOUS") 
+st.write("Hello, I'm Maurício Lacerda (some call me Mau), a lifelong learner with a knack for delving into the realm of data through business-oriented methodologies, tools, and languages tailored for digital enterprises. Fueled by insatiable curiosity and an energetic drive to deliver results, I thrive on integrating teams and individuals toward common objectives.")
