f913e9289009db24f0c3ab4c321e751c9d9c1bf0
Maurício Goi Lacerda <32021294+maupnx@users.noreply.github.com>
2024-04-02 09:39:53 -0300

    Update 4Academic_Background.py

--- pages/4Academic_Background.py
+++ pages/4Academic_Background.py
@@ -4,7 +4,7 @@ from PIL import Image
 import os
 import requests
 
-st.set_page_config(page_title="Academic Background", page_icon=":man_student:", layout="wide")
+st.set_page_config(page_title="Academic Background", page_icon=":books:", layout="wide")
 
 def display_image(url_or_path, width=100):
     try:
