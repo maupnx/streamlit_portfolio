e3c03e8cb1bb8cffe3e02d546ad3589db53f5326
Maurício Goi Lacerda <32021294+maupnx@users.noreply.github.com>
2024-04-02 09:38:19 -0300

    Update 5Datascience_Projects.py

--- pages/5Datascience_Projects.py
+++ pages/5Datascience_Projects.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_title="Data Science Projects", page_icon=":snake:", layout="wide")
+
 st.title("My Phyton Portfolio 🐍")
 st.subheader("*Under Development")
 st.write("https://github.com/maupnx")
